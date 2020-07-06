68a7421eb7643e297e1f66145444e8fbf1d5db11
Michał Szurgot <szurgi27@gmail.com>
2020-07-06 11:03:37 +0200

    test coding

--- Praktyka/guessinggame.py
+++ Praktyka/guessinggame.py
@@ -7,14 +7,24 @@ print("---------------------------------")
 print("Guess the number of M&Ms and you get lunch on the house!")
 print()
 
-
 mm_count = random.randint(1, 101)
 attempt_limit = 5
 attempts = 0
 while attempts < attempt_limit:
     guess_text = input("How many M&Ms are in the jar? ")
     guess = int(guess_text)
-    print(guess)
     attempts += 1
 
-print("Bye")
+    if mm_count == guess:
+        print(f"You got a free lunch! It was {guess}.")
+        break
+    elif mm_count > guess:
+        print("Sorry, this's too LOW!")
+    else:
+        print("That's too HIGH!")
+
+
+if attempts == 5:
+    print(f"Game Over \n Bye , you're done in {attempts}! \n corect is {mm_count}")
+else:
+    print(f"Bye, you're done in {attempts}!")
